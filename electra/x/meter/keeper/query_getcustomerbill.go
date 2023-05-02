8ef2067db64c54b38c057e555db8a914b70ad8a0
alkia <maxime@alkia.net>
2023-05-02 17:49:31 +0700

    GetcustomerbillRequest

--- electra/x/meter/keeper/query_getcustomerbill.go
+++ electra/x/meter/keeper/query_getcustomerbill.go
@@ -2,8 +2,15 @@ package keeper
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	"github.com/cosmos/cosmos-sdk/types/query"
 
 	"electra/x/meter/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -13,11 +20,46 @@ func (k Keeper) Getcustomerbill(goCtx context.Context, req *types.QueryGetcustom
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
+	// Performance Management
+	start := time.Now()
+	// DONE: Process the query
+	var customerbillinglines []types.Customerbillingline
+	var nblines uint64 = 0
+	var billTotalWh, billTotalPrice uint64 = 0, 0
+	var stDebug, currency string = "", "uelectra"
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	store := ctx.KVStore(k.storeKey)
+	customerbillinglineStore := prefix.NewStore(store, types.KeyPrefix(types.CustomerbillinglineKeyPrefix))
 
-	// TODO: Process the query
-	_ = ctx
+	pageRes, err := query.Paginate(customerbillinglineStore, req.Pagination, func(key []byte, value []byte) error {
+		var customerbillingline types.Customerbillingline
+		if err := k.cdc.Unmarshal(value, &customerbillingline); err != nil {
+			return err
+		}
 
-	return &types.QueryGetcustomerbillResponse{}, nil
+		if (req.CustomerDeviceID == customerbillingline.CustomerDeviceID) && (uint64(customerbillingline.CycleID) == uint64(req.BillCycleID)) {
+			customerbillinglines = append(customerbillinglines, customerbillingline)
+			billTotalWh += customerbillingline.LineWh
+			billTotalPrice += customerbillingline.LineWhPrice
+			nblines++
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	// Convert to array of json string
+	var displaybillinglines []string
+	for _, line := range customerbillinglines {
+		json, _ := json.Marshal(line)
+		displaybillinglines = append(displaybillinglines, string(json))
+	}
+
+	elapsed := time.Since(start)
+	stDebug = fmt.Sprintf("Search took %s", elapsed)
+	return &types.QueryGetcustomerbillResponse{Customerbillinglines: displaybillinglines, BillTotalWh: billTotalWh, BillTotalPrice: billTotalPrice, Currency: currency, Nblines: nblines, Comments: stDebug, Pagination: pageRes}, nil
+
+	// return &types.QueryGetcustomerbillResponse{}, nil
 }
--- electra/x/meter/types/query.pb.gw.go
+++ electra/x/meter/types/query.pb.gw.go
@@ -945,6 +945,10 @@ func local_request_Query_CustomerbillinglineAll_0(ctx context.Context, marshaler
 
 }
 
+var (
+	filter_Query_Getcustomerbill_0 = &utilities.DoubleArray{Encoding: map[string]int{"customerDeviceID": 0, "billCycleID": 1}, Base: []int{1, 1, 2, 0, 0}, Check: []int{0, 1, 1, 2, 3}}
+)
+
 func request_Query_Getcustomerbill_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq QueryGetcustomerbillRequest
 	var metadata runtime.ServerMetadata
@@ -976,6 +980,13 @@ func request_Query_Getcustomerbill_0(ctx context.Context, marshaler runtime.Mars
 
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "billCycleID", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_Getcustomerbill_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.Getcustomerbill(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -1014,6 +1025,13 @@ func local_request_Query_Getcustomerbill_0(ctx context.Context, marshaler runtim
 
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "billCycleID", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_Getcustomerbill_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.Getcustomerbill(ctx, &protoReq)
