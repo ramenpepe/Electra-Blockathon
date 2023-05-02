066ffd29925f5cd9f2d8cb7f2a74797eb94c7e28
alkia <maxime@alkia.net>
2023-05-02 08:15:03 +0700

    # # Create the record command to store the monophased meterreadings in the blockchain # #

--- electra/x/meter/module_simulation.go
+++ electra/x/meter/module_simulation.go
@@ -24,7 +24,11 @@ var (
 )
 
 const (
-// this line is used by starport scaffolding # simapp/module/const
+	opWeightMsgRecord = "op_weight_msg_record"
+	// TODO: Determine the simulation weight value
+	defaultWeightMsgRecord int = 100
+
+	// this line is used by starport scaffolding # simapp/module/const
 )
 
 // GenerateGenesisState creates a randomized GenState of the module
@@ -70,6 +74,17 @@ func (am AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
 // WeightedOperations returns the all the gov module operations with their respective weights.
 func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
 	operations := make([]simtypes.WeightedOperation, 0)
+
+	var weightMsgRecord int
+	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgRecord, &weightMsgRecord, nil,
+		func(_ *rand.Rand) {
+			weightMsgRecord = defaultWeightMsgRecord
+		},
+	)
+	operations = append(operations, simulation.NewWeightedOperation(
+		weightMsgRecord,
+		metersimulation.SimulateMsgRecord(am.accountKeeper, am.bankKeeper, am.keeper),
+	))
 
 	// this line is used by starport scaffolding # simapp/module/operation
 
