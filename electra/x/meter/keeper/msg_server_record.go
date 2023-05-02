8723284d5151ad7c32e39faf1c45560c31cc13d2
alkia <maxime@alkia.net>
2023-05-02 09:18:55 +0700

    // DONE: Handling the message

--- electra/x/meter/keeper/msg_server_record.go
+++ electra/x/meter/keeper/msg_server_record.go
@@ -4,14 +4,29 @@ import (
 	"context"
 
 	"electra/x/meter/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) Record(goCtx context.Context, msg *types.MsgRecord) (*types.MsgRecordResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// DONE: Handling the message
+	//var datetimeM = fmt.Sprintf("%d", ctx.BlockHeight())
+
+	var recording = types.Meterreadings{
+		DeviceID:  msg.Creator,
+		Timestamp: msg.Timestamp, //Timestamp:   uint64(ctx.BlockHeight()),
+		Phase:     msg.Phase,
+		Whin:      msg.Whin,
+		Whout:     msg.Whout,
+		Mvolt:     msg.Mvolt,
+		Mhertz:    msg.Mhertz,
+		Mpf:       msg.Mpf,
+		Maxmi:     msg.Maxmi,
+	}
+
+	k.SetMeterreadings(ctx, recording) // from scafolding the MAP object | in file keeper/meterreading.go
 
 	return &types.MsgRecordResponse{}, nil
 }
