9532ed096069e6d53adc7d73b40606b9db0433bb
kwangwei <kwangwei@sol59.com>
2023-05-03 09:30:09 +0800

    FE

---
+++ FE/src/components/Savings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function savings() {
+  return (
+    <div>
+      <p>Savings applet to show real time: grid price - traded price</p>
+    </div>
+  );
+}
+
+export default savings;
---
+++ FE/src/components/Trx.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function trx() {
+  return (
+    <div>
+      <h1>Trx C</h1>
+      <p>Feed for new trx on chain</p>
+    </div>
+  );
+}
+
+export default trx;
